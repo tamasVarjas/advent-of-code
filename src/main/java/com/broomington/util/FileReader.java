cf366f0ec35df3f8abbcc22d915177de5692cb3f
Tamás Varjas <tamas.varjas00@gmail.com>
2023-12-01 12:09:32 +0100

    Create FileReader util

---
+++ src/main/java/com/broomington/util/FileReader.java
@@ -0,0 +1,34 @@
+package com.broomington.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import static java.lang.String.format;
+
+public class FileReader {
+
+    private FileReader() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static <T> String getInput(T instance) throws IOException, ClassNotFoundException {
+        Class<?> clazz = instance.getClass();
+        final InputStream inputStream = clazz.getClassLoader().getResourceAsStream(format("%s-input.txt", clazz.getSimpleName()));
+
+        return readFromInputStream(inputStream);
+    }
+
+    private static String readFromInputStream(InputStream inputStream) throws IOException {
+        final StringBuilder sb = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+        }
+
+        return sb.toString();
+    }
+}
