adb66f7d12e48c8e2dcfd0d77876dd373fd6d955
Tamás Varjas <tamas.varjas00@gmail.com>
2025-10-25 20:25:45 +0200

    2024 Day 5, refactor to use records

--- src/main/java/com/broomington/twentytwentyfour/Day5.java
+++ src/main/java/com/broomington/twentytwentyfour/Day5.java
@@ -1,60 +1,111 @@
 package com.broomington.twentytwentyfour;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
+import java.util.stream.IntStream;
 
 import static com.broomington.util.FileReader.getInput;
-import static java.util.stream.Collectors.toList;
 
 public class Day5 {
 
     public static void main(final String[] args) throws IOException {
         final String input = getInput(2024, Day5.class);
-        final String[] rules = input.split("\n\n")[0]
-                .split("\n");
-        final String[] updates = input.split("\n\n")[1]
-                .split("\n");
+        final var rules = Arrays.stream(input.split("\n\n")[0]
+                        .split("\n"))
+                .map(Day5::toRule)
+                .toList();
+        final var updates = Arrays.stream(input.split("\n\n")[1]
+                .split("\n"))
+                .map(Day5::toUpdate)
+                .toList();
 
-        final List<Map<Integer, Integer>> pageByIndexPerUpdate = new ArrayList<>();
-        for (int i = 0; i < updates.length; i++) {
-            final Map<Integer, Integer> update = new HashMap<>();
-            pageByIndexPerUpdate.add(update);
-            final String[] pages = updates[i].split(",");
-            for (int j = 0; j < pages.length; j++) {
-                update.put(Integer.valueOf(pages[j]), j);
-            }
-        }
+        partOne(updates, rules);
+        //partTwo(updates, rules);
+    }
 
-        final List<Map<Integer, Integer>> correctUpdates = new ArrayList<>();
-        for (final Map<Integer, Integer> update : pageByIndexPerUpdate) {
-            boolean isCorrect = true;
-            for (final String rule : rules) {
-                final Integer first = Integer.valueOf(rule.split("\\|")[0]);
-                final Integer second = Integer.valueOf(rule.split("\\|")[1]);
-                if (update.containsKey(first) && update.containsKey(second) && update.get(first) > update.get(second)) {
-                    isCorrect = false;
-                    break;
-                }
-            }
-            if (isCorrect) {
-                correctUpdates.add(update);
-            }
-        }
-
-        int sumOfCorrectMiddles = 0;
-        for (final Map<Integer, Integer> update : correctUpdates) {
-            final int index = (update.size() - 1) / 2;
-            for (final Map.Entry<Integer, Integer> entry : update.entrySet()) {
-                if (entry.getValue() == index) {
-                    sumOfCorrectMiddles += entry.getKey();
-                }
-            }
-        }
+    private static void partOne(final List<Update> updates, final List<Rule> rules) {
+        final int sumOfCorrectMiddles = updates.stream()
+                .filter(update -> isCorrect(update, rules))
+                .map(Day5::getMiddlePage)
+                .mapToInt(PageUpdate::page)
+                .sum();
 
         System.out.println(sumOfCorrectMiddles);
+    }
+
+    private static PageUpdate getMiddlePage(final Update update) {
+        final int middleIndex = (update.pageUpdates.size() - 1) / 2;
+        return update.getPageUpdateByIndex(middleIndex);
+    }
+
+    /*
+    private static void partTwo(final List<Map<Integer, Integer>> pageByIndexPerUpdate, final List<Rule> rules) {
+        final List<Map<Integer, Integer>> incorrectUpdates = new ArrayList<>();
+        for (final Map<Integer, Integer> update : pageByIndexPerUpdate) {
+            if (!isCorrect(rules, update)) {
+                incorrectUpdates.add(update);
+            }
+        }
+
+        System.out.println("foo");
+    }
+     */
+
+    private static boolean isCorrect(final Update update, final List<Rule> rules) {
+        return rules.stream()
+                .filter(rule -> isRuleRelevant(update, rule))
+                .allMatch(rule -> isRuleSatisfied(update, rule));
+    }
+
+    private static boolean isRuleRelevant(final Update update, final Rule rule) {
+        final var pageUpdates = update.pageUpdates;
+        return pageUpdates.stream()
+                .anyMatch(pageUpdate -> pageUpdate.page() == rule.first)
+                && pageUpdates.stream()
+                .anyMatch(pageUpdate -> pageUpdate.page == rule.second);
+    }
+
+    private static boolean isRuleSatisfied(final Update update, final Rule rule) {
+        return update.getPageUpdateByPage(rule.first).index < update.getPageUpdateByPage(rule.second).index;
+    }
+
+    private static Rule toRule(final String ruleStr) {
+        final var ruleArr = ruleStr.split("\\|");
+        return new Rule(
+                Integer.parseInt(ruleArr[0]),
+                Integer.parseInt(ruleArr[1])
+        );
+    }
+
+    private static Update toUpdate(final String pageUpdateStr) {
+        final var pageUpdateArr = pageUpdateStr.split(",");
+        final var pageUpdates = IntStream.range(0, pageUpdateArr.length)
+                .mapToObj(i -> new PageUpdate(Integer.parseInt(pageUpdateArr[i]), i))
+                .toList();
+        return new Update(pageUpdates);
+    }
+
+    private record Rule(int first, int second) {
+    }
+
+    private record PageUpdate(int page, int index) {
+    }
+
+    private record Update(List<PageUpdate> pageUpdates) {
+
+        private PageUpdate getPageUpdateByPage(final int page) {
+            return pageUpdates.stream()
+                    .filter(pageUpdate -> pageUpdate.page == page)
+                    .findFirst()
+                    .orElseThrow();
+        }
+
+        private PageUpdate getPageUpdateByIndex(final int index) {
+            return pageUpdates.stream()
+                    .filter(pageUpdate -> pageUpdate.index == index)
+                    .findFirst()
+                    .orElseThrow();
+        }
     }
 }
